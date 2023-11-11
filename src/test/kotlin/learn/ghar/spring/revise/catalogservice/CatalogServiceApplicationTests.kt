2d7d25d1683dff719b4b84f106b28501909bf17b
sayyidnaqvi <naqvi.sdsu@gmail.com>
2023-11-11 00:29:59 -0600

    initial working app

---
+++ src/test/kotlin/learn/ghar/spring/revise/catalogservice/CatalogServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package learn.ghar.spring.revise.catalogservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
