7b5e21289b44cc70a3ac25b664e27a390f2f1388
sayyidnaqvi <naqvi.sdsu@gmail.com>
2023-11-11 12:45:18 -0600

    added controller, service classes, working

--- src/main/kotlin/learn/ghar/spring/revise/catalogservice/service/GreetingsService.kt
+++ src/main/kotlin/learn/ghar/spring/revise/catalogservice/service/GreetingsService.kt
@@ -2,8 +2,13 @@ package learn.ghar.spring.revise.catalogservice.service
 
 import org.springframework.stereotype.Service
 
+/**
+ * This annotation [@Service] serves as a specialization of @Component,
+ * allowing for implementation classes to be autodetected through classpath scanning.
+ *
+ * @author Abbas Naqvi
+ */
 @Service
 class GreetingsService {
-
     fun retrieveGreeting(name : String) = "Hi $name"
 }
