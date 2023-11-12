90f9228cc3289824f98e20f90bbbb954beed081a
sayyidnaqvi <naqvi.sdsu@gmail.com>
2023-11-12 00:58:35 -0600

    added logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,11 +20,17 @@ repositories {
 }
 
 dependencies {
+
+	val loggingVersion= "3.0.5"
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	//logging
+	implementation("io.github.microutils:kotlin-logging-jvm:$loggingVersion")
+
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -32,7 +38,7 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
+		freeCompilerArgs = freeCompilerArgs + "-Xjsr305=strict"
 		jvmTarget = "17"
 	}
 }
--- src/main/kotlin/learn/ghar/spring/revise/catalogservice/controller/GreetingController.kt
+++ src/main/kotlin/learn/ghar/spring/revise/catalogservice/controller/GreetingController.kt
@@ -1,6 +1,7 @@
 package learn.ghar.spring.revise.catalogservice.controller
 
 import learn.ghar.spring.revise.catalogservice.service.GreetingsService
+import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/v1/greetings")
 class GreetingController(val greetingsService: GreetingsService) {                  // service class-injection to 'controller
 
+    companion object : KLogging()
+
     @GetMapping("/{inputName}")
     fun retrieveGreeting(@PathVariable("inputName") name : String) : String{
+
+        logger.info { "Name is $name" }
         return greetingsService.retrieveGreeting(name)                              // invoking service-class to build the greeting
     }
 }
